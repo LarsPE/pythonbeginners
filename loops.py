4625f48a5757e206c90b3b0dbe20070451f2b7e6
Lars Emblem <lars.emblem@gmail.com>
2021-03-14 14:03:55 +0100

    added some files..

---
+++ loops.py
@@ -0,0 +1,26 @@
+# loops.py
+
+x = 0
+while x < 10:
+    print("The value of x is now: ", x)
+    x += 1
+
+for x in range(10):
+    print("x in the for loop: ", x)
+
+
+for x in range(10, 20):
+    print("x in the for loop: ", x)
+
+z = 0
+while True:
+    z += 1
+    print("Value of z: ", z)
+    if z == 6:
+        break
+
+v = 2
+if v == 25:
+    pass
+
+print("123")
--- statements.py
+++ statements.py
@@ -1,10 +1,12 @@
 # statements.py
 # Statements and conditions
 
-x = int(input("First number: "))
-y = int(input("Second number: "))
+x = int(input("First number (x): "))
+y = int(input("Second number (y): "))
 
 if x < y:
 	print("x is less than y")
+elif x > y:
+	print("x is bigger than y")
 else:
-	print("x bigger than y")
+	print("x is equal to y")
