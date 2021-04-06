6b4217ff0a1147d3d8314d949dead0f00626c79f
Lars Emblem <lars.emblem@gmail.com>
2021-04-06 16:54:28 +0200

    all good for beginner files

--- strings.py
+++ strings.py
@@ -10,3 +10,21 @@ print(text[1:4])
 
 for letter in text:
 	print(letter)
+
+newText = "hello world\nThis is a new day\nAnd a new line here...."
+print(newText)
+
+#string formatting
+name = "Lars"
+age = 45
+print("My name is %s and I am %d year old" % (name, age))
+print("My name is {} and I am {} year old".format(name, age))
+
+moreText = "This is some more text today"
+print(moreText.upper())
+print(moreText.lower())
+print(moreText.title())		#first letter uppercase in each word
+
+tekst = "I am Lars and my life is beautiful ! Because of my job"
+print(tekst.count('my'))
+print(tekst.count(' '))
